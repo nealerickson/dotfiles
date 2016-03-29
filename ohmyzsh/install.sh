b9585bd73f6a6b85636e8b2c5affd7a5b5dd7d71
Neal Erickson <neal@heedworks.com>
2016-03-29 16:33:59 -0400

    Add install script for Oh My Zsh

---
+++ ohmyzsh/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Oh My Zsh
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Oh My Zsh.
+
+# Check for Oh My Zsh
+if ! [ -d "${HOME}/.oh-my-zsh" ]
+  echo "  Installing Oh My Zsh for you."
+
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+fi
+
+exit 0
