262f564e5152cc2ad3b12b2d0a8dd42d18886e5e
Neal Erickson <neal@erickson.io>
2016-09-04 21:48:10 -0400

    Update emacs configuration for cask package manager

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -25,6 +25,3 @@ alias pcat='pygmetize'
 alias cbp='pbpaste|less'
 
 alias cpwd="pwd | tr -d '\n' | pbcopy"
-
-alias emacs='open -a /Applications/Emacs.app $1'
-alias ec='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient'
