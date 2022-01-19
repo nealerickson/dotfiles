8c2f60fc0febc20d0b5d25943344f8251d8db72d
Neal Erickson <neal@erickson.io>
2022-01-19 15:26:52 -0500

    Update docker aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,7 +1,6 @@
 alias d='docker $*'
 alias d-c='docker-compose $*'
 
-# Docker
 alias docker-nuke="docker ps -a -q | xargs docker stop && docker ps -a -q | xargs docker rm && docker images -a -q | xargs docker rmi -f"
-# alias docker-nuke-volumes="docker volume rm `docker volume ls -qf dangling=true`"
-# alias docker-stop-all="docker ps -a -q | xargs docker stop"
+alias docker-nuke-volumes="docker volume rm `docker volume ls -qf dangling=true`"
+alias docker-stop-all="docker ps -a -q | xargs docker stop"
